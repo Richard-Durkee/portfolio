d9987c63ef10a311fa3450095bbc1844366ee27b
Richard Durkee <richarddurkee@Richards-Mac-mini.local>
2024-05-13 13:30:06 -0400

    Projects on Home Page

--- richarddurkee.com/src/app/page.tsx
+++ richarddurkee.com/src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import NavBar from "./navbar";
 import SideNav from "./sidenav";
+import ProjectLinks from "./projects/projects-nav-links";
 
 export default function Home() {
   return (
     <main>
-      
+      <div>
+        <p className="mb-4">Projects</p>
+        <ProjectLinks></ProjectLinks>
+      </div>
     </main>
   );
 }
