ae117246ece683a878a3dcfd229329e34e66970f
Richard Durkee <richarddurkee1@gmail.com>
2024-05-15 19:26:48 -0400

    fixed rendering bug with medium sized screens again

--- richarddurkee.com/src/app/content.tsx
+++ richarddurkee.com/src/app/content.tsx
@@ -27,7 +27,7 @@ export default function Content({ content }: Props) {
             return (
                 <>
                     <div key={index} className='flex flex-col sm:flex-row items-center md:space-x-8 space-y-8 bg-zinc-900 rounded-3xl p-9 mb-4'> 
-                        <div className='flex-shrink-0 max-w-[50%] sm:max-w-[100%] max-h-[500px] overflow-auto rounded-xl'>
+                        <div className='flex-shrink-0 md:max-w-[50%] max-h-[500px] overflow-auto rounded-xl'>
                             <Image src={`${block.image}`} alt={block.alt} width={500} height={500}/>
                         </div>
                         <div className='flex-grow'>
@@ -40,12 +40,12 @@ export default function Content({ content }: Props) {
         }
         else {
             return (
-                <div key={index} className='flex flex-col sm:flex-row items-center md:space-x-8 space-y-8 bg-zinc-900 rounded-3xl p-9 mb-4'> 
+                <div key={index} className='flex flex-col sm:flex-row sm:items-center md:space-x-8 space-y-8 bg-zinc-900 rounded-3xl p-9 mb-4'> 
                     <div className='flex-grow'>
                         <h2 className='text-xl mb-4'> {block.heading} </h2>
-                        <p className="text-gray-200 max-h-[500px] overflow-auto mb-4"> {block.text} </p>
+                        <p className="text-gray-200 max-h-[500px] overflow-auto"> {block.text} </p>
                     </div>
-                    <div className='flex-shrink-0 max-w-[50%] sm:max-w-[100%] max-h-[500px] overflow-auto rounded-xl'>
+                    <div className='flex-shrink-0 md:max-w-[50%] max-h-[500px] overflow-auto rounded-xl'>
                         <Image src={block.image} alt={block.alt} width={500} height={500}/>
                     </div>
                 </div>
