99e60ffd8ec7ba5a26c07bd585ba21854b122e48
Richard Durkee <richarddurkee1@gmail.com>
2024-05-15 12:34:13 -0400

    Driver AI content format

--- richarddurkee.com/src/app/projects/spothouse/page.tsx
+++ richarddurkee.com/src/app/projects/spothouse/page.tsx
@@ -1,9 +1,4 @@
 import Image from 'next/image';
-import SpotHouseLogo from '/public/images/spothouse/spothouse-logo.png'
-import SpotHouseHost from '/public/images/spothouse/spothouse-host.png';
-import SpotHouseGuest from '/public/images/spothouse/spothouse-guest.png'
-import SpotHouseSigmoidEquation from '/public/images/spothouse/spothouse-sigmoid-equation.png'
-import SpotHouseSigmoidGraph from '/public/images/spothouse/spothouse-sigmoid-graph.png';
 import Content from '@/app/content';
 import SpothouseContent from '../../../../public/content/spothouse-content.json'
 
