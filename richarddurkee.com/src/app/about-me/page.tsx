765433019f16a01fe50e4770f96754bb7825d6b8
Richard Durkee <richarddurkee1@gmail.com>
2024-05-27 11:47:48 -0400

    Fixed alignment of About-Me Page, with small text changes

--- richarddurkee.com/src/app/about-me/page.tsx
+++ richarddurkee.com/src/app/about-me/page.tsx
@@ -5,15 +5,9 @@ export default function Page() {
     return (
       <div> 
         <h1 className='text-xl mb-4'>About Me</h1>
-        <div className='flex'>
+        <div className='flex flex-col md:flex-row items-center'>
           <Image src={HeadShot} alt="Headshot" className='rounded-3xl max-w-[50%] m-4'/>
-          <p className=''> My name is Richard F. Durkee, or Rick, 
-
-and confident relying on self-taught methods to realize my passion projects, alone I set-up my small-scale server and began configuration for novel operations, excitedly and securely hosting burner websites and creating a streaming device
-
-.My wondering self-instruction taught me the foundation of transcoding in microserver networks as well as 
-
-</p>
+          <p className=''> My name is Richard F. Durkee, or Rick, and I am curious, user-focused, and self-motivated. relying on self-taught methods to realize my passion projects, alone I set-up my small-scale server and began configuration for novel operations, excitedly and securely hosting burner websites and creating a streaming device. My wondering self-instruction taught me the foundation of transcoding in microserver networks. </p>
         </div>
       </div>);
   }
