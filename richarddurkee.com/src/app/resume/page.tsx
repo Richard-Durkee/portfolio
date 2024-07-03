ceb8be7bdf82c0e3d431a60e0cf46781178d6e77
Richard Durkee <richarddurkee1@gmail.com>
2024-07-03 10:08:44 -0400

    Fixing Alt Text for Download PDF Button

--- richarddurkee.com/src/app/resume/page.tsx
+++ richarddurkee.com/src/app/resume/page.tsx
@@ -8,7 +8,6 @@ export default function Page() {
         <div className="flex flex-col w-[50%] justify-center"> 
           <Link
             href="/pdfs/richard-durkee-resume.pdf"
-            alt="alt text"
             target="_blank"
             rel="noopener noreferrer"
             className="flex w-[50%] h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm text-white font-medium hover:bg-sky-100 hover:text-blue-600 sm:mx-3 md:mx-4 lg:mx-5"
